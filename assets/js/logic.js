8305f5305f8e495eca6cf299e3451e153a7be4fa
Vilma Hernandez <vilmaglynn@hotmail.com>
2023-12-22 15:01:37 -0600

    added code timer and start button

---
+++ assets/js/logic.js
@@ -0,0 +1,212 @@
+const scores = document.querySelector(".scores");
+const hide = document.querySelector(".hide");
+const choices = document.querySelector(".choices");
+const feedback = document.querySelector(".feedback");
+
+// let timerElement = document.getElementById("time");
+const timerElement = document.querySelector("#time");
+const startButton = document.querySelector("#start");
+const startScreen = document.querySelector("#start-screen");
+const questionTitle = document.querySelector("#question-title");
+const choicesid = document.querySelector("#choices");
+const finalScore = document.querySelector("#final-score");
+const submit = document.querySelector("#submit");
+
+// const wordBlank = document.querySelector(".word-blanks");
+// const win = document.querySelector(".win");
+// const lose = document.querySelector(".lose");
+// const timerElement = document.querySelector(".timer-count");
+// const startButton = document.querySelector(".start-button");
+
+let chosenWord = "";
+let numBlanks = 0;
+let winCounter = 0;
+let loseCounter = 0;
+let isWin = false;
+let timer;
+let timerCount;
+
+// Arrays used to create blanks and letters on screen
+let lettersInChosenWord = [];
+let blanksLetters = [];
+
+// Array of words the user will guess
+const words = ["constiable","array", "modulus", "object", "function", "string", "boolean"];
+
+// The init function is called when the page loads 
+function init() {
+  getWins();
+  getlosses();
+}
+
+// The startGame function is called when the start button is clicked
+function startGame() {
+
+hide.style.display = "block";
+startScreen.classList.add('hide');
+  isWin = false;
+  timerCount = 60;
+  // Prevents start button from being clicked when round is in progress
+//   startButton.disabled = true;
+//   renderBlanks()
+  startTimer()
+
+}
+
+// The winGame function is called when the win condition is met
+function winGame() {
+  wordBlank.textContent = "YOU WON!!!🏆 ";
+  winCounter++
+  startButton.disabled = false;
+  setWins()
+}
+
+// The loseGame function is called when timer reaches 0
+function loseGame() {
+  wordBlank.textContent = "GAME OVER";
+  loseCounter++
+  startButton.disabled = false;
+  setLosses()
+}
+
+// The setTimer function starts and stops the timer and triggers winGame() and loseGame()
+function startTimer() {
+  // Sets timer
+
+  timer = setInterval(function() {
+    timerCount--;
+    timerElement.textContent = timerCount;
+    if (timerCount >= 0) {
+      // Tests if win condition is met
+      if (isWin && timerCount > 0) {
+        // Clears interval and stops timer
+        clearInterval(timer);
+        winGame();
+      }
+    }
+    // Tests if time has run out
+    if (timerCount === 0) {
+      // Clears interval
+      clearInterval(timer);
+      loseGame();
+    }
+  }, 1000);
+}
+
+// Creates blanks on screen
+function renderBlanks() {
+  // Randomly picks word from words array
+  chosenWord = words[Math.floor(Math.random() * words.length)];
+  lettersInChosenWord = chosenWord.split("");
+  numBlanks = lettersInChosenWord.length;
+  blanksLetters = []
+  // Uses loop to push blanks to blankLetters array
+  for (let i = 0; i < numBlanks; i++) {
+    blanksLetters.push("_");
+  }
+  // Converts blankLetters array into a string and renders it on the screen
+  wordBlank.textContent = blanksLetters.join(" ")
+}
+
+// Updates win count on screen and sets win count to client storage
+function setWins() {
+  win.textContent = winCounter;
+  localStorage.setItem("winCount", winCounter);
+}
+
+// Updates lose count on screen and sets lose count to client storage
+function setLosses() {
+  lose.textContent = loseCounter;
+  localStorage.setItem("loseCount", loseCounter);
+}
+
+// These functions are used by init
+function getWins() {
+  // Get stored value from client storage, if it exists
+  const storedWins = localStorage.getItem("winCount");
+  // If stored value doesn't exist, set counter to 0
+  if (storedWins === null) {
+    winCounter = 0;
+  } else {
+    // If a value is retrieved from client storage set the winCounter to that value
+    winCounter = storedWins;
+  }
+  //Render win count to page
+  win.textContent = winCounter;
+}
+
+function getlosses() {
+  const storedLosses = localStorage.getItem("loseCount");
+  if (storedLosses === null) {
+    loseCounter = 0;
+  } else {
+    loseCounter = storedLosses;
+  }
+  lose.textContent = loseCounter;
+}
+
+function checkWin() {
+  // If the word equals the blankLetters array when converted to string, set isWin to true
+  if (chosenWord === blanksLetters.join("")) {
+    // This value is used in the timer function to test if win condition is met
+    isWin = true;
+  }
+}
+
+// Tests if guessed letter is in word and renders it to the screen.
+function checkLetters(letter) {
+  let letterInWord = false;
+  for (let i = 0; i < numBlanks; i++) {
+    if (chosenWord[i] === letter) {
+      letterInWord = true;
+    }
+  }
+  if (letterInWord) {
+    for (let j = 0; j < numBlanks; j++) {
+      if (chosenWord[j] === letter) {
+        blanksLetters[j] = letter;
+      }
+    }
+    wordBlank.textContent = blanksLetters.join(" ");
+  }
+}
+
+// Attach event listener to document to listen for key event
+document.addEventListener("keydown", function(event) {
+  // If the count is zero, exit function
+  if (timerCount === 0) {
+    return;
+  }
+  // Convert all keys to lower case
+  const key = event.key.toLowerCase();
+  const alphabetNumericCharacters = "abcdefghijklmnopqrstuvwxyz0123456789 ".split("");
+  // Test if key pushed is letter
+  if (alphabetNumericCharacters.includes(key)) {
+    const letterGuessed = event.key;
+    checkLetters(letterGuessed)
+    checkWin();
+  }
+});
+
+// Attach event listener to start button to call startGame function on click
+startButton.addEventListener("click", startGame);
+
+// Calls init() so that it fires when page opened
+init();
+
+// Bonus: Add reset button
+const resetButton = document.querySelector(".reset-button");
+
+function resetGame() {
+  // Resets win and loss counts
+  winCounter = 0;
+  loseCounter = 0;
+  // Renders win and loss counts and sets them into client storage
+  setWins()
+  setLosses()
+}
+// // Attaches event listener to button
+// resetButton.addEventListener("click", resetGame);
+
+// Attaches event listener to button
+resetButton.addEventListener("click", resetGame);
